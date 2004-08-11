5701456756a091429d5175f71810782fe59865b3
genevb <>
2004-08-11 21:26:38 +0000

    Trade static arrays for vectors

--- StSecondaryVertexMaker/StV0FinderMaker.h
+++ StSecondaryVertexMaker/StV0FinderMaker.h
@@ -9,14 +9,14 @@
 #define StV0FinderMaker_hh
 
 #include "StMaker.h"
-#include "StThreeVectorD.hh"
+#include "StPhysicalHelixD.hh"
+#include <vector>
 
 class St_ev0_ev0par2;
 class ev0_ev0par2_st;
 class StEvent;
 class StV0Vertex;
 class StTrack;
-class StPhysicalHelixD;
 
 
 //Betty
@@ -191,8 +191,6 @@ class StV0FinderMaker : public StMaker {
   double ptV0sq;                   //! 
   double Bfield;		   //!
   unsigned short trks;		   //!
-  unsigned short ptrks;		   //!
-  unsigned short ntrks;		   //!
   Bool_t prepared;		   //!
   Bool_t useExistingV0s;           //!
   Bool_t dontZapV0s;		   //!
@@ -208,17 +206,21 @@ class StV0FinderMaker : public StMaker {
   int ITTFflag;		           //!
   int TPTflag;                     //!
   
-  int maxtracks;                   //!
-  StTrack** trk;                   //!
-  unsigned short* ntrk;            //!
-  unsigned short* ptrk;            //!
-  short* hits;                     //!
-  int* detId;                      //!
-  double* pt;                      //!
-  double* ptot;                    //!
-  unsigned short *trkID;           //!
-  StPhysicalHelixD* heli;          //!
+  vector<StTrack*> trk;            //!
+  vector<unsigned short> ntrk;     //!
+  vector<unsigned short> ptrk;     //!
+  vector<short> hits;              //!
+  vector<int> detId;               //!
+  vector<double> pt;               //!
+  vector<double> ptot;             //!
+  vector<StPhysicalHelixD> heli;   //!
+  vector<unsigned short> trkID;    //!
+  void ExpandVectors(unsigned short size);
+
   StThreeVectorD mainv;            //!
+
+ private:
+  static StV0FinderMaker* mInstance;      //!
 
   ClassDef(StV0FinderMaker,0)
 
@@ -229,6 +231,9 @@ class StV0FinderMaker : public StMaker {
 //_____________________________________________________________________________
 // $Id$
 // $Log$
+// Revision 1.8  2004/04/02 08:56:22  faivre
+// Use actual TPT flag rather than "not ITTF" for TPT tracks.
+//
 // Revision 1.7  2004/03/03 01:46:13  cmironov
 // (J.L. commit)
 // Modified doxygen documentation (was not taken before). Indentation
