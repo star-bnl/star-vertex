d0b84e206dfcc20363d6c14f319d75ef99decad0
fisyak <>
2006-10-17 13:38:03 +0000

    Remove dependencies from dead classes

--- StGenericVertexMaker/StiPPVertex/StPPVertexFinder.cxx
+++ StGenericVertexMaker/StiPPVertex/StPPVertexFinder.cxx
@@ -30,7 +30,6 @@
 #include "Sti/StiKalmanTrackNode.h"
 #include "StiMaker/StiMaker.h"
 #include "Sti/StiTrackContainer.h"
-#include "Sti/StiTrackToTrackMap.h"
 
 #include "St_db_Maker/St_db_Maker.h"
 #include "StIOMaker/StIOMaker.h" //tmp
@@ -1122,6 +1121,9 @@ StPPVertexFinder::matchTrack2Membrane(const StiKalmanTrack* track,TrackData &t){
 /**************************************************************************
  **************************************************************************
  * $Log$
+ * Revision 1.24  2006/07/31 17:57:54  balewski
+ * cleanup before 2006 prod, no changes in algo, CTB stays out all the time
+ *
  * Revision 1.23  2006/06/02 20:46:55  perev
  * Accoun DCA node added
  *
