4e9dd2f9a93d787032c291695158a08abe369472
smirnovd <>
2016-11-07 21:19:14 +0000

    Moved log print statements out of constructors

--- StGenericVertexMaker/Minuit/StMinuitVertexFinder.cxx
+++ StGenericVertexMaker/Minuit/StMinuitVertexFinder.cxx
@@ -47,7 +47,6 @@ StMinuitVertexFinder::setExternalSeed(const StThreeVectorD& s)
 
 StMinuitVertexFinder::StMinuitVertexFinder(VertexFit_t fitMode) : StGenericVertexFinder(fitMode)
 {
-  LOG_INFO << "StMinuitVertexFinder::StMinuitVertexFinder is in use." << endm;
   mBeamHelix =0;
   
   mMinuit = new TMinuit(3);         
