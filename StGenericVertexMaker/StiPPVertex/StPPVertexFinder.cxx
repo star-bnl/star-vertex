4e4f016f9e0c57db5e4ab9d51470fc3779c0ee10
smirnovd <>
2017-01-20 17:49:20 +0000

    StPPVertexFinder: Copy found vertices to final container regardless of the fitting mode

--- StGenericVertexMaker/StiPPVertex/StPPVertexFinder.cxx
+++ StGenericVertexMaker/StiPPVertex/StPPVertexFinder.cxx
@@ -964,14 +964,6 @@ int StPPVertexFinder::fitTracksToVertex(VertexData &vertex) const
 //-------------------------------------------------
 void 
 StPPVertexFinder::exportVertices(){
-  if ( mVertexFitMode != VertexFit_t::Beamline1D &&
-       mVertexFitMode != VertexFit_t::Beamline3D )
-  {
-    // code is not ready for reco w/o beamLine
-    LOG_FATAL << "StPPVertexFinder code is not ready for reco w/o beamLine" << endm;
-    assert(mVertexFitMode == VertexFit_t::Beamline1D ||
-           mVertexFitMode == VertexFit_t::Beamline3D);
-  }
 
   for (const VertexData &V : mVertexData)
   {
