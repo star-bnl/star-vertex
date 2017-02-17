19e69f80062113394bf2889eadde06a7bd8cc810
smirnovd <>
2017-02-17 21:29:32 +0000

    StMinuitVertexFinder: Removed unused code

--- StGenericVertexMaker/Minuit/StMinuitVertexFinder.h
+++ StGenericVertexMaker/Minuit/StMinuitVertexFinder.h
@@ -159,8 +159,6 @@ private:
     Float_t                mZMin;             // Min z of possible vertex positions
     Float_t                mZMax;             // Max z of possible vertex positions
 
-    StPhysicalHelixD*      mBeamHelix;        // Beam Line helix
-    
     enum                   {maxSeed=500};
 
     /// The number of vertex seeds found in current event
@@ -170,7 +168,6 @@ private:
     Int_t                  mBemcHit[120][20][2];  // modules, eta, sub
     static std::vector<StPhysicalHelixD> mHelices;
     static std::vector<UShort_t>         mHelixFlags;
-    static std::vector<Double_t>         mSigma;
     static std::vector<Double_t>         mZImpact;
     //static std::vector<Bool_t>         mCTB;
     static Bool_t                   requireCTB;
